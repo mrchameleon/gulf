3859a993e595599ba7872d73aee91315e5d7fe21
Ryan Johnson <4967884-mrchameleon@users.noreply.gitlab.com>
2025-02-14 09:50:40 -0500

    sort of works, but not well. in the right direction at least

--- gulf.js
+++ gulf.js
@@ -4,15 +4,19 @@ const viewportHeight = window.innerHeight;
 // console.log(`Viewport width: ${viewportWidth}px`);
 // console.log(`Viewport height: ${viewportHeight}px`);
 
-const gulf_lat = 26.0;
-const gulf_lng = -88.7;
+// taken at zoom level 8
+const gulf_lat = 25.2220721;
+const gulf_lng = -90.064935;
 
+// percent to cover w/ zoom level 8?
+const APPROX_CENTER_LAT_POS = 46;
+const APPROX_CENTER_LNG_POS = 46;
 
 var overlay = document.createElement("div"); overlay.style.position = "fixed"; overlay.style.top = "0"; overlay.style.left = "0"; overlay.style.width = "100%"; overlay.style.height = "100%"; overlay.style.backgroundColor = "rgba(33, 34, 33, 0.0)"; overlay.style.zIndex = "100"; overlay.style.pointerEvents = "none"; document.body.appendChild(overlay);
 var para = document.createElement("p");
 
 
-var centered_tmp = "https://www.google.com/maps/@25.9924091,-91.4927232,5z?entry=ttu&g_ep=EgoyMDI1MDIxMS4wIKXMDSoASAFQAw%3D%3D";
+var centered_tmp = "https://www.google.com/maps/@25.2220721,-90.064935,8z?entry=ttu&g_ep=EgoyMDI1MDIxMS4wIKXMDSoASAFQAw%3D%3D";
 if (window.location != centered_tmp) {
   window.location.replace(centered_tmp);
 } else {
@@ -31,14 +35,14 @@ function overlay_init() {
 
   // need a better formula to get starting position based on users current pos/center
   // lat, lng, zoom and
-  para.style.left = "48%";
-  para.style.top = "50%";
+  para.style.left = APPROX_CENTER_LAT_POS + "%";
+  para.style.top = APPROX_CENTER_LNG_POS + "%";
   para.style.verticalAlign = "center";
   para.style.marginLeft = "100% auto";
   para.style.marginRight = "100% auto";
   para.style.padding = "10px";
   para.style.margin = "4px";
-  para.style.border = "2px solid black;"
+  para.style.border = "1px solid black";
   para.style.color = "black";
   para.style.fontSize = "1.5em"
   para.style.textShadow = "-1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff;";
@@ -76,34 +80,39 @@ const observeUrlChange = () => {
       console.log("left_pct current", left_pct);
       console.log("top_pct current", top_pct);
       
-  
+      
+      // try multipler
+      // bigger % moves when zoom value is HIGHER
+      // lower % moves when zoom value is LOWER.
+
+
       if (lat < gulf_lat) {
         console.log("panned south")
         var lat_delta = (gulf_lat - lat);
         console.log("lat_delta: (s)", lat_delta);
-        para.style.top = (48 - lat_delta) + "%";
-        console.log("SET top to ", (48 - lat_delta), "%");
+        para.style.top = (APPROX_CENTER_LAT_POS - lat_delta) + "%";
+        console.log("SET top to ", (APPROX_CENTER_LAT_POS - lat_delta), "%");
         
       } else {
         console.log("panned north")
         var lat_delta = (lat - gulf_lat);
         console.log("lat_delta (n):", lat_delta);
-        para.style.top = (48 + lat_delta) + "%";
-        console.log("SET top to ", (48 + lat_delta), "%");
+        para.style.top = (APPROX_CENTER_LAT_POS + lat_delta) + "%";
+        console.log("SET top to ", (APPROX_CENTER_LAT_POS + lat_delta), "%");
       }
   
       if (lng < gulf_lng) {
         console.log("panned west")
-        var lng_delta = (Math.abs(lng) - Math.abs(gulf_lng)) ;
+        var lng_delta = (Math.abs(lng) - Math.abs(gulf_lng));
         console.log("lng_delta (w)", lng_delta);
-        para.style.left = (50 + lng_delta) + "%";
-        console.log("SET left to ", (50 - lng_delta), "%");
+        para.style.left = (APPROX_CENTER_LNG_POS + lng_delta) + "%";
+        console.log("SET left to ", (APPROX_CENTER_LNG_POS + lng_delta), "%");
       } else {
         console.log("panned east")
         var lng_delta = (Math.abs(gulf_lng) - Math.abs(lng)) ;
         console.log("lng_delta (e))", lng_delta);
-        para.style.left = (50 + lng_delta) + "%";
-        console.log("SET left to ", (50 - lng_delta), "%");
+        para.style.left = (APPROX_CENTER_LNG_POS - lng_delta) + "%";
+        console.log("SET left to ", (APPROX_CENTER_LNG_POS - lng_delta), "%");
       }
 
 
